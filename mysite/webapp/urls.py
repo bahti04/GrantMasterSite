54bcc7f0f0517333c38bc87e9e5272f6706e6b93
bahti04 <137329792+bahti04@users.noreply.github.com>
2024-07-09 01:33:23 +0500

    Delete mysite directory

--- mysite/webapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('before_translation/', views.before_translation, name='before_translation'),
-    path('after_translation/', views.after_translation, name='after_translation'),
-]
